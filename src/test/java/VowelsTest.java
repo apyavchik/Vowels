986a6083604f98b82a88dea0491e5b463d7215f5
apyavchik <a.pyavchik@gmail.com>
2021-03-25 17:42:18 +0200

    add implementation add more tests

--- src/test/java/VowelsTest.java
+++ src/test/java/VowelsTest.java
@@ -8,4 +8,14 @@ class VowelsTest {
         assertEquals("Nope!", 5, Vowels.getCount("abracadabra"));
     }
 
+    @Test
+    public void testCase2() {
+        assertEquals("Nope!", 5, Vowels.getCount("AbracAdabra"));
+    }
+
+    @Test
+    public void testCase3() {
+        assertEquals("Nope!", 6, Vowels.getCount("Aleksander Pyavchik"));
+    }
+
 }
